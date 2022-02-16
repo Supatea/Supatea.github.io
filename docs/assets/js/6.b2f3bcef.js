8d36cdd662ddf4706334cb724ca8d52f4f76d41c
aros <aros.yeh@collaborate.tw>
2022-02-16 12:31:52 +0800

    update test

---
+++ docs/assets/js/6.b2f3bcef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{749:function(t,n,e){"use strict";e.r(n);var s=e(109),i=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("p",{staticClass:"demo"},[this._v("This is another component")])}),[],!1,null,null,null);n.default=i.exports}}]);
